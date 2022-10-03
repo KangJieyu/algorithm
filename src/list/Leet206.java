88a1854478753135a6f46cb01ff5b79014b83327
KangJieyu <2220406910@qq.com>
2022-10-03 18:37:41 +0800

    leetcode206.反转链表,ListNode类中添加create方法

---
+++ src/list/Leet206.java
@@ -0,0 +1,36 @@
+package list;
+
+/**
+ * 206. 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ * @Date: 2022/10/3 14:13
+ * @Author: KangJieyu
+ */
+public class Leet206 {
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // 保护头节点
+        ListNode node = new ListNode(-1);
+        node.next = head;
+        // pre固定不变
+        ListNode pre = head;
+        ListNode post = pre.next;
+        while (post != null) {
+            pre.next = post.next;
+            post.next = node.next;
+            node.next = post;
+            post = pre.next;
+        }
+        return node.next;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.create(arr);
+        ListNode node = reverseList(head);
+        System.out.println(node);
+    }
+}
--- src/list/ListNode.java
+++ src/list/ListNode.java
@@ -11,4 +11,28 @@ class ListNode {
         val = x;
         next = null;
     }
+
+    /**
+     * 根据数组元素创建链表
+     * @param arr 数组中的元素依次代表链表中的元素
+     * @return 返回创建链表的头节点
+     */
+    public static ListNode create(int[] arr) {
+        // 判空操作
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        // 以数组第一个元素为头节点
+        ListNode head = new ListNode(arr[0]);
+        // 创建当前节点，用于连接的中间节点
+        ListNode cur = head;
+        // 从第二个元素开始遍历数组，创建节点，指定指针
+        for (int i = 1; i < arr.length; i++) {
+            // 创建i索引元素的节点
+            ListNode node = new ListNode(arr[i]);
+            cur.next = node;
+            cur = node;
+        }
+        return head;
+    }
 }
